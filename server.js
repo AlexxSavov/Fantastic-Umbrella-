8e3545b58da7e0108ab3b34fdaf24d939916abd0
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-08-09 20:49:11 -0400

    syntax

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-const { ExclusionConstraintError } = require('sequelize');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
